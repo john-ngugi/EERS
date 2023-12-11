9f116e2f37b44f788e9961b8ebb6a917734f3030
LUCIA <luciawanjiru165@gmail.com>
2023-12-11 16:24:48 +0300

    adding comments

--- ResponseApp/admin.py
+++ ResponseApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import *
+from .models import *  # Import all models from the current directory
+
 # Register your models here.
 
 admin.site.register(Coordinates)
-admin.site.register(Resources)
+admin.site.register(FireResources)
+admin.site.register(MedicalResourses)
+admin.site.register(PoliceResources)
--- ResponseApp/models.py
+++ ResponseApp/models.py
@@ -2,23 +2,45 @@ from django.db import models
 import uuid
 # Create your models here.
 
+# Define a Django model for Fire Resources
+class FireResources(models.Model):
+    fire = models.IntegerField(default=34) # Default value for the 'fire' resource
+    location_name = models.CharField(max_length=50,null=True)   # Name of the location
+    rfid =  models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4,  # Generate a unique identifier using UUID4
+         editable = False)     # The identifier should not be editable   
+    
+    def __str__(self):
+        return str(self.rfid)  # String representation for easy identification
 
-class Resources(models.Model):
-    fire = models.IntegerField(default=34)
+# Define a Django model for Medical Resources
+class MedicalResourses(models.Model):
     medical = models.IntegerField(default=34)
-    crime = models.IntegerField(default=34)
+    location_name = models.CharField(max_length=50,null=True)
     rfid =  models.UUIDField( 
          primary_key = True, 
          default = uuid.uuid4, 
          editable = False)
     
     def __str__(self):
-        return self.rfid  
+        return str(self.rfid)
+    
+# Define a Django model for Police Resources
+class PoliceResources(models.Model):
+    police = models.IntegerField(default=34)
+    location_name = models.CharField(max_length=50,null=True)
+    rfid =  models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4, 
+         editable = False)
+    def __str__(self):
+        return str(self.rfid)
+# Define a Django model for Coordinates
 class Coordinates(models.Model):
-    x_coordinate = models.DecimalField(max_digits=2,decimal_places=2)
-    y_coordinate = models.DecimalField(max_digits=2,decimal_places=2)
-    name = models.CharField(max_length=100)
+    x_coordinate =  models.CharField(max_length=50,null=True)  # X-coordinate value
+    y_coordinate =  models.CharField(max_length=50,null=True)  # Y-coordinate value
+    name = models.CharField(max_length=100)  # Name for the coordinates
 
     def __str__(self):
         return self.name
-
